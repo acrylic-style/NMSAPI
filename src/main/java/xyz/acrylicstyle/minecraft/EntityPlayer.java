9f6c229feb748c865089e31b58b7a2e82ddb3d91
acrylic-style <me@acrylicstyle.xyz>
2020-02-27 02:34:24 +0900

    added polling, but not recommended.

--- src/main/java/xyz/acrylicstyle/minecraft/EntityPlayer.java
+++ src/main/java/xyz/acrylicstyle/minecraft/EntityPlayer.java
@@ -1,7 +1,11 @@
 package xyz.acrylicstyle.minecraft;
 
+import com.google.common.collect.Lists;
+import org.apache.commons.lang.Validate;
 import org.bukkit.Location;
 import org.bukkit.WeatherType;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import util.CollectionList;
 import util.ReflectionHelper;
 import xyz.acrylicstyle.authlib.GameProfile;
@@ -14,16 +18,41 @@ import java.util.List;
 
 @SuppressWarnings("unused")
 public class EntityPlayer {
+    private static boolean doPolling = false;
+    private boolean disposed = false;
+    private Plugin plugin = null;
     private Object o;
     public PlayerConnection playerConnection;
+    public int ping = -1;
+    public final MinecraftServer server = MinecraftServer.getMinecraftServer(getField("server"));
+    public List<Integer> removeQueue = Lists.newLinkedList();
+    public String locale = "en_us";
+    public int invulnerableTicks = 60;
+    public String displayName = "";
+    public long timeOffset;
+    public boolean relativeTime;
+    public float pluginRainPosition;
+    public float pluginRainPositionPrevious;
+    public Location compassTarget = null;
+    public Integer clientViewDistance = 0;
+    public boolean viewingCredits;
+    public boolean sentListPacket;
+    public boolean joining;
+    public double maxHealthCache;
+    public boolean keepLevel;
+    public int newExp = 0;
+    public int newLevel = 0;
+    public int newTotalExp = 0;
+    public boolean e;
+    public int containerCounter;
+    public int cp;
+    public boolean cq;
+    public boolean cm;
+    public boolean ci = true;
 
     public EntityPlayer(Object o) {
         this.o = o;
         this.playerConnection = new PlayerConnection(this);
-    }
-
-    public static EntityPlayer fromCraftPlayer(CraftPlayer player) {
-        return new EntityPlayer(player);
     }
 
     public void reset() {
@@ -67,6 +96,55 @@ public class EntityPlayer {
     }
 
     // NMSAPI start
+    public EntityPlayer setPlugin(Plugin plugin) {
+        this.plugin = plugin;
+        return this;
+    }
+
+    public static EntityPlayer fromCraftPlayer(CraftPlayer player) {
+        return new EntityPlayer(player);
+    }
+
+    /**
+     * Cancel the polling.
+     */
+    public void cancelPolling() { disposed = true; }
+
+    public void poll() {
+        Validate.notNull(plugin, "Plugin cannot be null");
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (disposed) this.cancel();
+                EntityPlayer.this.ping = getPing();
+                EntityPlayer.this.removeQueue = getRemoveQueue();
+                EntityPlayer.this.locale = getLocale();
+                EntityPlayer.this.invulnerableTicks = getInvulnerableTicks();
+                EntityPlayer.this.displayName = getDisplayName();
+                EntityPlayer.this.timeOffset = (long) getField("timeOffset");
+                EntityPlayer.this.relativeTime = getRelativeTime();
+                EntityPlayer.this.pluginRainPosition = getPluginRainPosition();
+                EntityPlayer.this.pluginRainPositionPrevious = getPluginRainPositionPrevious();
+                EntityPlayer.this.compassTarget = getCompassTarget();
+                EntityPlayer.this.clientViewDistance = getClientViewDistance();
+                EntityPlayer.this.viewingCredits = getViewingCredits();
+                EntityPlayer.this.sentListPacket = getSentListPacket();
+                EntityPlayer.this.joining = getJoining();
+                EntityPlayer.this.maxHealthCache = getMaxHealthCache();
+                EntityPlayer.this.keepLevel = getKeepLevel();
+                EntityPlayer.this.newExp = (int) getField("newExp");
+                EntityPlayer.this.newLevel = (int) getField("newLevel");
+                EntityPlayer.this.newTotalExp = (int) getField("newTotalExp");
+                EntityPlayer.this.e = getE();
+                EntityPlayer.this.containerCounter = (int) getField("containerCounter");
+                EntityPlayer.this.cp = (int) getField("cp");
+                EntityPlayer.this.cq = (boolean) getField("cq");
+                EntityPlayer.this.cm = (boolean) getField("cm");
+                EntityPlayer.this.ci = (boolean) getField("ci");
+            }
+        }.runTaskTimer(plugin, 0, 2);
+    }
+
     public Object getEntityPlayer() {
         try {
             if (o.getClass().getCanonicalName().startsWith("net.minecraft.server") && o.getClass().getCanonicalName().endsWith("EntityPlayer")) return o;
@@ -78,7 +156,7 @@ public class EntityPlayer {
         }
     }
 
-    public String getLocaleField() {
+    public String getLocale() {
         return (String) getField("locale");
     }
 
