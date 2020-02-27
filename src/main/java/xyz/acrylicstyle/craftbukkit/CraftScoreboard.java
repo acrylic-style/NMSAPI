abacdb0c729ebef18524b8dbfeed99d09a31387b
acrylic-style <me@acrylicstyle.xyz>
2020-02-27 14:40:40 +0900

    Fix CraftScoreboard

--- src/main/java/xyz/acrylicstyle/craftbukkit/CraftScoreboard.java
+++ src/main/java/xyz/acrylicstyle/craftbukkit/CraftScoreboard.java
@@ -2,4 +2,15 @@ package xyz.acrylicstyle.craftbukkit;
 
 import org.bukkit.scoreboard.Scoreboard;
 
-public abstract class CraftScoreboard implements Handler<xyz.acrylicstyle.minecraft.Scoreboard>, Scoreboard {}
+public abstract class CraftScoreboard implements Handler<xyz.acrylicstyle.minecraft.Scoreboard>, Scoreboard {
+    private Scoreboard scoreboard;
+
+    public CraftScoreboard(Scoreboard scoreboard) {
+        this.scoreboard = scoreboard;
+    }
+
+    @Override
+    public xyz.acrylicstyle.minecraft.Scoreboard getHandle() {
+        return new xyz.acrylicstyle.minecraft.Scoreboard(scoreboard);
+    }
+}
